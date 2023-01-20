6a0a23969a6e7f6e3fe7b5a4611aa108865e2d66
Shinichiro <nuno1216shin@gmail.com>
2023-01-20 19:46:08 +0900

    lesson8 変数

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 //定数
 
-// console.log(150 * 140)
-// console.log(150 * 160)
+let price = 150;
+console.log(price * 140);
+console.log(price * 160);
 
-const price = 150;
-
-console.log(price * 140)
-console.log(price * 160)
+price = 170;
+console.log(price * 140);
+console.log(price * 160);
