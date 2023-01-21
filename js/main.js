cad2dabc0062ed4ad9ab5ba9fcd5781deb367e97
Shinichiro <nuno1216shin@gmail.com>
2023-01-21 12:56:02 +0900

    lesson17 for文

--- js/main.js
+++ js/main.js
@@ -1,32 +1,6 @@
 'use strict';
 
-// const score = 60;
-// const name = 'taguchi';
-
-// // if (score >= 50) {
-// //     if (name === 'taguchi') {
-// //         console.log('Good job!');
-// //     }
-// // }
-
-// if (score >= 50 && name === 'taguchi') {
-//     console.log('Good job!');
-// }
-
-const signal = 'pink';
-
-switch(signal) {
-    case 'red':
-        console.log('Stop!');
-        break;
-    case 'yellow':
-        console.log('Caution!');
-        break;
-    case 'blue':
-    case 'green':
-        console.log('Go!');
-        break;
-    default:
-        console.log('Wrong signal')
-        break;
+for (let i = 1; i <= 10; i++) {
+    // console.log('hello');
+    console.log(`hello ${i}`);
 }
