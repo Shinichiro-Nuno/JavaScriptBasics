5cb41e2a3edcb8fa703eead935af2c81e33ccc8a
Shinichiro <nuno1216shin@gmail.com>
2023-01-21 20:12:26 +0900

    lesson20 関数

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 'use strict';
 
-for (let i = 1; i <= 10; i++) {
-    // if (i === 4) {
-    // if (i % 3 === 0) {
-    //     continue;
-    // }
-    if (i === 4) {
-        break;
-    }
-    console.log(i);
+function showAd() {
+console.log('----------');
+console.log('--- Ad ---');
+console.log('----------');
 }
+
+// console.log('----------');
+// console.log('--- Ad ---');
+// console.log('----------');
+showAd();
+console.log('Tom is great!');
+console.log('Tom is great!');
+// console.log('----------');
+// console.log('--- Ad ---');
+// console.log('----------');
+showAd();
+console.log('Tom is great!');
+console.log('Tom is great!');
+showAd();
+// console.log('----------');
+// console.log('--- Ad ---');
+// console.log('----------');
