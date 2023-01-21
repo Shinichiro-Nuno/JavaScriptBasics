f1d15113a4aed6799b9ba022e1d6585472814d08
Shinichiro <nuno1216shin@gmail.com>
2023-01-21 12:49:29 +0900

    lesson16 switch文

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 'use strict';
 
-const score = 60;
-const name = 'taguchi';
+// const score = 60;
+// const name = 'taguchi';
 
-// if (score >= 50) {
-//     if (name === 'taguchi') {
-//         console.log('Good job!');
-//     }
+// // if (score >= 50) {
+// //     if (name === 'taguchi') {
+// //         console.log('Good job!');
+// //     }
+// // }
+
+// if (score >= 50 && name === 'taguchi') {
+//     console.log('Good job!');
 // }
 
-if (score >= 50 && name === 'taguchi') {
-    console.log('Good job!');
+const signal = 'pink';
+
+switch(signal) {
+    case 'red':
+        console.log('Stop!');
+        break;
+    case 'yellow':
+        console.log('Caution!');
+        break;
+    case 'blue':
+    case 'green':
+        console.log('Go!');
+        break;
+    default:
+        console.log('Wrong signal')
+        break;
 }
