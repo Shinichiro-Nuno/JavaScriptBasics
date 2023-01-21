5171b220789a2662c89c6c49707b5aa238966df9
Shinichiro <nuno1216shin@gmail.com>
2023-01-21 10:46:07 +0900

    lesson12 比較演算子 Boolean

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 'use strict';
 
-// console.log('5' * 3);
-// console.log('5' - 3);
+const price = 1200;
 
-// console.log(parseInt('5', 10) + 3);
+// console.log(price > 1000);
+// console.log(price < 1000);
+// console.log(price >= 1000);
+// console.log(price <= 1000);
+// console.log(price === 1000);
+// console.log(price !== 1000);
 
-console.log(parseInt('hello', 10));
+
+console.log(Boolean(0));
+console.log(Boolean('hello'));
