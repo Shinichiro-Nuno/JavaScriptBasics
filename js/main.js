0e2b44f4f288b36c38756f1d790287f52e61e881
Shinichiro <nuno1216shin@gmail.com>
2023-01-21 12:42:09 +0900

    lesson15 論理演算子

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const score = 85;
+const score = 60;
+const name = 'taguchi';
 
-// if (score >= 80) {
-//     console.log('Great!');
-// } else if (score >= 60) {
-//     console.log('Good.');
-// } else {
-//     console.log('OK...');
+// if (score >= 50) {
+//     if (name === 'taguchi') {
+//         console.log('Good job!');
+//     }
 // }
 
-score >= 80 ? console.log('Great!') : console.log('OK...');
+if (score >= 50 && name === 'taguchi') {
+    console.log('Good job!');
+}
