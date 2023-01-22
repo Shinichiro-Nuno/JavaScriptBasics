0bf88701081d8a2bf3280d644362437df9457b9a
Shinichiro <nuno1216shin@gmail.com>
2023-01-22 12:43:51 +0900

    変数のコピー

--- js/main.js
+++ js/main.js
@@ -7,8 +7,14 @@
     // console.log(x);
     // console.log(y);
 
+    // let x = [1, 2];
+    // let y = x;
+    // x[0] = 5;
+    // console.log(x);
+    // console.log(y);
+
     let x = [1, 2];
-    let y = x;
+    let y = [...x];
     x[0] = 5;
     console.log(x);
     console.log(y);
