39f254edb750b8528758cfd41980fe142df74bd9
Shinichiro <nuno1216shin@gmail.com>
2023-01-23 20:28:56 +0900

    カプセル化

--- js/main.js
+++ js/main.js
@@ -8,16 +8,22 @@
     }
 
     show() {
-      console.log(`${this.text} - ${this.likeCount}いいね`);
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+    like() {
+      this.likeCount++;
+      this.show();
     }
   }
-
+  
 
   const posts = [
     new Post('JavaScriptの勉強中・・・'),
     new Post('プログラミング楽しい！'),
   ];
 
-  posts[0].show();
-  posts[1].show();
+  posts[0].like();
+
+  // posts[0].show();
+  // posts[1].show();
 }
